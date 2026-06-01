40c3bb86b75b27261d1cfe90df157095c86e2440
KedaiVPN <caracarayt0@gmail.com>
2026-06-01 07:21:21 +0700

    Update RetrofitClient.kt

--- kasir-android/core/src/main/java/com/kasirinaja/core/network/RetrofitClient.kt
+++ kasir-android/core/src/main/java/com/kasirinaja/core/network/RetrofitClient.kt
@@ -7,7 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "https://api
+.free-account.my.id/api/"
 
     private var tokenProvider: (() -> String?)? = null
 
